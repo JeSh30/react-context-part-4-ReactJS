845640313c56b550cb845105f5dc592484fc63e5
JeSh30 <rajeshanumala1230@gmail.com>
2023-07-26 10:05:40 +0000

    solution

---
+++ src/components/Cart/index.js
@@ -0,0 +1,31 @@
+import Header from '../Header'
+import CartListView from '../CartListView'
+import CartContext from '../../context/CartContext'
+import EmptyCartView from '../EmptyCartView'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      const showEmptyCart = cartList.length === 0
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyCart ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <CartListView />
+              </div>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
